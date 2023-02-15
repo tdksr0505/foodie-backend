de445c9c39e8db5036f887136b23daa52b1e1917
DESKTOP-1AKMAGM\Lucas <tdksr0505@gmail.com>
2023-02-15 23:28:01 +0800

    feat: 新增vercel.json

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const MongoDb = require('./mongodb');
 const app = express();
 const mongodb = new MongoDb();
 const PORT = 8080;
-app.get('/restaurants', async function (req, res) {
+app.get('/restaurant', async function (req, res) {
   const restaurants = await mongodb.getRestaurants();
   res.send({
     code: 200,
