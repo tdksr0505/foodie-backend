a07b941720539265d615f7ac9e87bc8a51ec7713
Lucas.Liu <Lucas.Liu@skylinetw.com>
2023-02-16 09:41:16 +0800

    feat: 完成新增 / 編輯 / 刪除

--- api/index.js
+++ api/index.js
@@ -1,63 +1,77 @@
-const app = require('express')();
-const MongoDb = require('../lib/mongodb');
+const express = require("express");
+const MongoDb = require("../lib/mongodb");
 const mongodb = new MongoDb();
 const PORT = 8080;
+const app = express();
 
+app.use(express.json());
 app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
-app.get('/api/restaurant', async (req, res) => {
-  //查詢
-  const restaurants = await mongodb.getRestaurants();
+app.get("/api/restaurant", async (req, res) => {
+  //查詢 全部
+  const result = await mongodb.getRestaurants();
   res.send({
     code: 200,
     data: {
-      restaurants,
+      restaurant: result,
     },
   });
 });
+app.get("/api/restaurant/:id", async (req, res) => {
+  // 查詢 單一
+  const { id } = req.params;
+  const result = await mongodb.getRestaurantDetail(id);
+  res.status(200).json({ code: 0, data: result });
+});
 
-app.post('/api/restaurant', async (req, res) => {
+app.post("/api/restaurant", async (req, res) => {
   //新增
-  const date = new Date();
-  const createDate = `${date.getFullYear()}-${(date.getMonth() + 1)
-    .toString()
-    .padStart(2, '0')}-${date
-    .getDate()
-    .toString()
-    .padStart(
-      2,
-      '0'
-    )} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-  const result = await restaurants.insertOne({
-    ...req.body,
-    createDate: createDate,
-  });
-  if (result.acknowledged) {
+  const result = await mongodb.createRestaurant(req.body);
+  if (result && result.acknowledged) {
     res.status(200).json({
       code: 0,
-      data: { msg: '已成功新增' },
+      data: { msg: "已成功新增" },
     });
   } else {
     res.status(200).json({
       code: 1,
-      data: { msg: '新增失敗' },
+      data: { msg: "新增失敗" },
     });
   }
 });
 
-app.get('/api/restaurant/:slug', async (req, res) => {
-  // GET 拿資料
-  const { slug } = req.params;
-  const result = await mongodb.getRestaurantDetail(slug);
-  res.status(200).json({ code: 0, data: result });
+app.post("/api/restaurant/:id", async (req, res) => {
+  //編輯
+  const { id } = req.params;
+  const result = await mongodb.updateRestaurant(id, req.body);
+  if (result && result.acknowledged && result.modifiedCount === 1) {
+    res.status(200).json({ code: 0, data: { msg: "已成功更新 " } });
+  } else {
+    res.status(200).json({ code: 1, data: { msg: "更新失敗 " } });
+  }
+});
+
+app.delete("/api/restaurant/:id", async (req, res) => {
+  //刪除
+  const { id } = req.params;
+  const result = await mongodb.deleteRestaurant(id);
+  if (result.acknowledged) {
+    res.status(200).json({
+      code: 0,
+      data: { msg: "已成功刪除" },
+    });
+  } else {
+    res.status(200).json({
+      code: 1,
+      data: { msg: "刪除失敗" },
+    });
+  }
 });
 
 app.listen(PORT);
 console.log(`[LAUNCH] Listening on port ${PORT}`);
-
-module.exports = app;
