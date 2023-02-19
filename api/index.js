68ee21014a12b1fa8d6efa4c12f2b89ac9a38c5e
DESKTOP-1AKMAGM\Lucas <tdksr0505@gmail.com>
2023-02-19 18:58:38 +0800

    feat: 新增身分驗證api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const MongoDb = require('../lib/mongodb');
+const jwt = require('jsonwebtoken');
+
 const mongodb = new MongoDb();
 const PORT = 8080;
 const app = express();
+const SECRET_KEY = process.env.JWT_SECRET_KEY;
+const expiresIn = '7d';
+
+const createToken = (payload) => {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
+};
+
+const verifyToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    console.log(decoded);
+    return decoded;
+  } catch (error) {
+    // err
+    console.error(error);
+    return null;
+  }
+};
 
 app.use(express.json());
 app.use((req, res, next) => {
@@ -26,7 +46,8 @@ app.get('/api/restaurant/:id', async (req, res) => {
   // 查詢 單一
   const { id } = req.params;
   const result = await mongodb.getRestaurantDetail(id);
-  res.status(200).json({ code: 0, data: result });
+  const code = result ? 0 : 1;
+  res.status(200).json({ code, data: result });
 });
 
 app.post('/api/restaurant', async (req, res) => {
@@ -72,6 +93,35 @@ app.delete('/api/restaurant/:id', async (req, res) => {
       data: { msg: '刪除失敗' },
     });
   }
+});
+
+app.post('/api/login', async (req, res) => {
+  //登入
+
+  const result = await mongodb.getMember(req.body);
+  console.log(result);
+  if (result) {
+    const token = createToken({ account: result.account });
+    res.status(200).json({
+      code: 0,
+      data: { account: result.account, token, msg: '登入成功' },
+    });
+  } else {
+    res.status(200).json({
+      code: 1,
+      data: { msg: '登入失敗' },
+    });
+  }
+});
+
+app.post('/api/auth/verify', async (req, res) => {
+  //jwt 驗證
+  const token = req.body.token;
+  const result = verifyToken(token);
+  res.status(200).json({
+    code: result ? 0 : 1,
+    data: { result },
+  });
 });
 
 app.listen(PORT);
